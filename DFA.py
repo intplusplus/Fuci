77c83e3355adb5591f694bfc49b62db50694ccaa
morelo <519144435@qq.com>
2018-07-22 11:13:08 +0800

    添加从字符串生成DFANode的功能
    添加从字符串生成DFANode的功能
    修改了一个bug，之前匹配的函数，内部使用的transition，忘记打self，导致依赖成了外部测试的transition

--- DFA.py
+++ DFA.py
@@ -21,7 +21,8 @@ class DFANode:
         assert self.nowstate in self.status
         
         match_success = False
-        next_status = transition[self.nowstate].get(char,None)
+        # print('char {0} now: {1} getnext'.format(char,self.nowstate))
+        next_status = self.transition[self.nowstate].get(char,None)
         
         next_status = None if next_status not in self.status else next_status
 
@@ -49,12 +50,23 @@ class DFANode:
         if match_string_success: 
             print('match success : ' , self.finish[self.nowstate])
         
+        print('match error')
         self._reinit()
         return match_string_success
 
     def _reinit(self):
         self.ismathed = False
         self.nowstate = self.status[0]
+
+    @classmethod
+    def from_string(cls,string,token_name):
+        chars = list(string)
+        alphabet = list(set(chars))
+        status = list(range(len(chars) + 1))
+        finish = {status[-1]:token_name}
+        transition = dict(zip(status,[{j:i} for i,j in enumerate(chars,start=1)]+[{}]))
+        return DFANode(alphabet = alphabet,status = status, finish = finish, transition = transition)
+
 
     def __or__(self,dfanode):
         assert isinstance(dfanode , (DFANode,DFA))
@@ -64,8 +76,9 @@ class DFANode:
         assert isinstance(dfanode ,(DFANode,DFA))
         return DFA(self,dfanode,'&')
 
-    def __repr__(self):
-        return 'DFANode'
+    # def __str__(self):
+    #     return '''  finish:  {0}  
+    #                 transition: {1}'''.format(str(self.finish),str(self.transition)), 
 
 class DFA:
 
@@ -113,7 +126,15 @@ transition = {
 node = DFANode(alphabet =list("0123456789."),status=list("1234567"),finish=dict(zip(list("2457"),['整数','小数','整数','小数'])),transition = transition)
 # node.match('124.12')
 
-i = node | node 
-i = i | node
-i.match('12.1')
+# i = DFANode.from_string('IF','IF')
 
+# print(i.nowstate)
+# print(i.finish)
+# print(i.transition)
+# i.match('IF')
+
+i = DFANode.from_string('I00','I00')
+i.match('I')
+print(i.nowstate)
+print(i.finish)
+print(i.transition)
