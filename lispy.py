26ace8e5973f6a19ec1801a997d5167ee46a8f67
int++ <519144435@qq.com>
2018-08-07 11:28:03 +0800

    lispy
    ## (c) Peter Norvig, 2010-16; See http://norvig.com/lispy.html
    ## 学习 https://zhuanlan.zhihu.com/p/29186794
    
    学习文章,复制了一个简单的lisp解释器

--- lispy.py
+++ lispy.py
@@ -1,6 +1,9 @@
 import math
 import  operator as op
 
+## (c) Peter Norvig, 2010-16; See http://norvig.com/lispy.html
+## 学习 https://zhuanlan.zhihu.com/p/29186794
+
 
 Symbol = str
 List = list
@@ -51,7 +54,23 @@ def read_from_tokens(tokens,begin):
             L.append(value)
             # print(stack)
 
-ast = parse('(begin (define r 10) (* pi (* r r) ) )')
+# ast = parse('(begin (define r 10) (* pi (* r r) ) )')
+
+class Procedure(object):
+    "用户定义的Scheme过程。"
+    def __init__(self, parms, body, env):
+        self.parms, self.body, self.env = parms, body, env
+    def __call__(self, *args): 
+        return eval(self.body, Env(self.parms, args, self.env))
+
+class Env(dict):
+    "环境是以{'var':val}为键对的字典，它还带着一个指向外层环境的引用。"
+    def __init__(self, parms=(), args=(), outer=None):
+        self.update(zip(parms, args))
+        self.outer = outer
+    def find(self, var):
+        "寻找变量出现的最内层环境。"
+        return self if (var in self) else self.outer.find(var)
 
 def standard_env():
     
@@ -87,7 +106,7 @@ def standard_env():
     del env['__package__']
     del env['__loader__']
     del env['__spec__']
-    print(env)
+    # print(env)
     return env
 
 global_env = standard_env()
@@ -96,9 +115,12 @@ def eval(x, env=global_env):
     "对在某个环境下的表达式进行求值"
 
     if isinstance(x, Symbol):      # 变量引用
-        return env[x]
+        return env.find(x)[x]      ##  先找到x所在的环境，在取值！！！
     elif not isinstance(x, List):  # 字面常量
-        return x                
+        return x   
+    elif x[0] == 'quote':          #引用
+        (_,exp) = x
+        return exp             
     elif x[0] == 'if':             # 条件
         (_, test, conseq, alt) = x
         exp = (conseq if eval(test, env) else alt)
@@ -106,7 +128,29 @@ def eval(x, env=global_env):
     elif x[0] == 'define':         # 定义
         (_, var, exp) = x
         env[var] = eval(exp, env)
+    elif x[0] == 'set!':          #赋值
+        (_,var,exp) = x
+        env.find(var)[var] = eval(exp,env)
+    elif x[0] == 'lambda':        #匿名过程
+        (_,parms,body) = x
+        return Procedure(parms,body,env)
     else:                          # 过程调用
-        proc = eval(x[0], env)
+        proc = eval(x[0], env) ##x[0]是函数名，从env里取出函数
         args = [eval(arg, env) for arg in x[1:]]
         return proc(*args)
+
+def repl(prompt='lis.py> '):
+    "REPL的懒人实现。"
+    while True:
+        val = eval(parse(input(prompt)))
+        if val is not None: 
+            print(schemestr(val))
+
+def schemestr(exp):
+    "将一个Python对象转换回可以被Scheme读取的字符串。"
+    if isinstance(exp, List):
+        return '(' + ' '.join(map(schemestr, exp)) + ')' 
+    else:
+        return str(exp)
+
+repl()
